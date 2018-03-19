171994bd0cd237cdfbe85cce745c50f22292e7a5
小鱼周凌宇 <coderfish@163.com>
2018-03-19 17:21:37 +0800

    文件读写加锁

--- SparrowSDK/Classes/Core/SPRCacheManager.m
+++ SparrowSDK/Classes/Core/SPRCacheManager.m
@@ -8,7 +8,21 @@
 #import "SPRCacheManager.h"
 #import "SPRApi.h"
 
+static char *QueueName = "com.zhoulingyu.sparrow.queue";
+@interface SPRCacheManager () {
+    dispatch_queue_t _queue;
+}
+
+@end
+
 @implementation SPRCacheManager
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _queue = dispatch_queue_create(QueueName, DISPATCH_QUEUE_CONCURRENT);
+    }
+    return self;
+}
 
 - (NSArray *)apis {
     if (_apis == nil) {
@@ -47,71 +61,144 @@
 }
 
 + (void)cacheApis:(NSArray<SPRApi *> *)apis {
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:apis];
-    BOOL result = [data writeToFile:[self apisPath] atomically:YES];
+    [[SPRCacheManager sharedInstance] cacheApis:apis];
+}
 
-    if (result == NO) {
-        NSLog(@"Caching API failed");
-        return;
-    }
-    SPRCacheManager *manager = [SPRCacheManager sharedInstance];
-    manager.apis = apis;
+- (void)cacheApis:(NSArray<SPRApi *> *)apis {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSData *data = [NSKeyedArchiver archivedDataWithRootObject:apis];
+            BOOL result = [data writeToFile:[SPRCacheManager apisPath] atomically:YES];
+
+            if (result == NO) {
+                NSLog(@"Caching API failed");
+                return;
+            }
+            strongSelf.apis = apis;
+        }
+    });
+
 }
 
 + (NSArray<SPRApi *> *)getApisFromCache {
-    NSData *data = [[NSData alloc]initWithContentsOfFile:[self apisPath]];
-    NSArray<SPRApi *> *apis =  [NSKeyedUnarchiver unarchiveObjectWithData:data];
-    return apis;
+    __block NSArray<SPRApi *> *block_apis = [NSArray array];
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+    [[SPRCacheManager sharedInstance] getApisFromCache:^(NSArray<SPRApi *> *apis) {
+        block_apis = apis;
+        dispatch_semaphore_signal(sema);
+    }];
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+    return block_apis;
+}
+
+- (void)getApisFromCache:(void (^)(NSArray<SPRApi *> *apis))callback {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSData *data = [[NSData alloc]initWithContentsOfFile:[SPRCacheManager apisPath]];
+            NSArray<SPRApi *> *apis =  [NSKeyedUnarchiver unarchiveObjectWithData:data];
+            callback(apis);
+        }
+    });
 }
 
 + (void)clearApisFromCache {
-    NSFileManager *fileMger = [NSFileManager defaultManager];
-    NSString *path = [self apisPath];
+    [[self sharedInstance] clearApisFromCache];
+}
 
-    BOOL exist = [fileMger fileExistsAtPath:path];
-    if (exist) {
-        NSError *err;
-        [fileMger removeItemAtPath:path error:&err];
-        if (err) {
-            NSLog(@"删除 Projects 缓存失败");
-        } else {
-            [SPRCacheManager sharedInstance].apis = nil;
+- (void)clearApisFromCache {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSFileManager *fileMger = [NSFileManager defaultManager];
+            NSString *path = [SPRCacheManager apisPath];
+
+            BOOL exist = [fileMger fileExistsAtPath:path];
+            if (exist) {
+                NSError *err;
+                [fileMger removeItemAtPath:path error:&err];
+                if (err) {
+                    NSLog(@"删除 Projects 缓存失败");
+                } else {
+                    strongSelf.apis = nil;
+                }
+            }
         }
-    }
+    });
 }
 
 + (void)cacheProjects:(NSSet<SPRProject *> *)projects {
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:projects];
-    BOOL result = [data writeToFile:[self projectsPath] atomically:YES];
+    [[SPRCacheManager sharedInstance] cacheProjects:projects];
+}
 
-    if (result == NO) {
-        NSLog(@"Caching API failed");
-        return;
-    }
-    SPRCacheManager *manager = [SPRCacheManager sharedInstance];
-    manager.projects = projects;
+- (void)cacheProjects:(NSSet<SPRProject *> *)projects {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSData *data = [NSKeyedArchiver archivedDataWithRootObject:projects];
+            BOOL result = [data writeToFile:[SPRCacheManager projectsPath] atomically:YES];
+
+            if (result == NO) {
+                NSLog(@"Caching API failed");
+                return;
+            }
+            strongSelf.projects = projects;
+        }
+    });
 }
 
 + (NSSet<SPRProject *> *)getProjectsFromCache {
-    NSData *data = [[NSData alloc]initWithContentsOfFile:[self projectsPath]];
-    NSSet<SPRProject *> *projects =  [NSKeyedUnarchiver unarchiveObjectWithData:data];
-    return projects;
+    __block NSSet<SPRProject *> *block_projects = [NSSet set];
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+    [[SPRCacheManager sharedInstance] getProjectsFromCache:^(NSSet<SPRProject *> *projects) {
+        block_projects = projects;
+        dispatch_semaphore_signal(sema);
+    }];
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+    return block_projects;
+}
+
+- (void)getProjectsFromCache:(void (^)(NSSet<SPRProject *> *projects))callback {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSData *data = [[NSData alloc]initWithContentsOfFile:[SPRCacheManager projectsPath]];
+            NSSet<SPRProject *> *projects =  [NSKeyedUnarchiver unarchiveObjectWithData:data];
+            callback(projects);
+        }
+    });
 }
 
 + (void)clearProjectsFromCache {
-    NSFileManager *fileMger = [NSFileManager defaultManager];
-    NSString *path = [self projectsPath];
+    [[SPRCacheManager sharedInstance] clearProjectsFromCache];
+}
 
-    BOOL exist = [fileMger fileExistsAtPath:path];
-    if (exist) {
-        NSError *err;
-        [fileMger removeItemAtPath:path error:&err];
-        if (err) {
-            NSLog(@"删除 Projects 缓存失败");
-        } else {
-            [SPRCacheManager sharedInstance].apis = nil;
+- (void)clearProjectsFromCache {
+    __weak __typeof(self)weakSelf = self;
+    dispatch_sync(_queue, ^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (strongSelf) {
+            NSFileManager *fileMger = [NSFileManager defaultManager];
+            NSString *path = [SPRCacheManager projectsPath];
+
+            BOOL exist = [fileMger fileExistsAtPath:path];
+            if (exist) {
+                NSError *err;
+                [fileMger removeItemAtPath:path error:&err];
+                if (err) {
+                    NSLog(@"删除 Projects 缓存失败");
+                } else {
+                    strongSelf.projects = nil;
+                }
+            }
         }
-    }
+    });
 }
 
 + (NSString *)apisPath {
